17187bfa5366af4b45d5d8281fe93f90ffe916bd
尹望嵩 <yinwangsong@iktapp.com>
2025-01-17 08:42:04 +0000

    nits

--- deployment/mllm/android/app/src/main/java/org/saltedfish/chatbot/model.kt
+++
@@ -1,7 +0,0 @@
-package org.saltedfish.chatbot
-enum class MessageType{
-    TEXT,IMAGE,
-}
-data class Message(var text:String, val isUser:Boolean, val timeStamp:Int, val type:MessageType=MessageType.TEXT, var content:Any?=null, var isStreaming:Boolean=true, var id:Int=-1,){
-
-}
